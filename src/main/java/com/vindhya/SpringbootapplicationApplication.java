92557416fae9af87cfe564c616432475f4059e35
vindh <vindhyajee5@gmail.com>
2023-01-06 12:41:09 +0530

    adding logs properties

--- src/main/java/com/vindhya/SpringbootapplicationApplication.java
+++ src/main/java/com/vindhya/SpringbootapplicationApplication.java
@@ -4,13 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @RestController
 public class SpringbootapplicationApplication {
 
+	 Logger logger = LoggerFactory.getLogger(SpringbootapplicationApplication.class);
 	@RequestMapping("/")
     String hello() {
+		logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
         return "Hello World, Spring Boot!";
     }
 
